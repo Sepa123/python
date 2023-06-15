033cc20f01ae0d58962baf56292418e772a14d45
Sepa123 <sebastian.retamal.contreras@gmail.com>
2023-06-15 12:36:29 -0400

    update

--- routers/productos.py
+++ routers/productos.py
@@ -62,7 +62,8 @@ async def producto_picking_id(producto_id : str):
 @router.get("/buscar/sku/{codigo_sku}",status_code=status.HTTP_202_ACCEPTED)
 async def get_productos_sku(codigo_sku : str):
     results = conn.read_producto_sku(codigo_sku)
+    print(results)
     if not results:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Error con la consulta")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="SKU no encontrado")
     json_data = convert_to_json(results)
     return json_data
