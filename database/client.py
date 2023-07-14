6ae7ca5d8b74225ae6a3d7c8c99faffeeecdb9e1
Sepa123 <sebastian.retamal.contreras@gmail.com>
2023-07-14 14:38:39 -0400

    Update client.py

--- database/client.py
+++ database/client.py
@@ -1773,7 +1773,8 @@ class reportesConnection():
             cur.execute("""
             -- WMS
             select nro_carga,count(distinct(entrega)) from public.ti_wms_carga_easy_paso
-            where to_char(created_at,'yyyymmdd')=to_char(current_date,'yyyymmdd')
+            WHERE to_char(created_at,'yyyy-mm-dd hh24:mi')  >= to_char((obtener_dia_anterior() + INTERVAL '17 hours 30 minutes'),'yyyy-mm-dd hh24:mi')
+            AND to_char(created_at,'yyyy-mm-dd') <= to_char(CURRENT_DATE,'yyyy-mm-dd')
             group by 1
             order by 1 asc
             """)
