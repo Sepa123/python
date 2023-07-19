66d73252755e3fea6c89c9b97f99ba384440cdca
Sepa123 <sebastian.retamal.contreras@gmail.com>
2023-07-19 11:18:54 -0400

    Update linea_producto.py

--- database/schema/rutas/linea_producto.py
+++ database/schema/rutas/linea_producto.py
@@ -1,13 +1,22 @@
 def recuperar_linea_producto_schema(results):
     json_data = []
-    keys = ["Cliente", "Ingreso_cliente", "Recepcion", "Planificacion","en_ruta","Producto_entregado"]
+    lineas = []
     values = results[0][0].split(",")
-    data = {}
-    for k, v in zip(keys, values):
-        v = v.strip("\"")
-        if k == "Cliente" or k == "Producto_entregado":
-            v = v.strip('()\"" ')
-        data[k] = v
-    json_data.append(data)
 
-    return json_data
+    print(values)
+    for i in range(1,6):
+        lineas.append(values[i].strip('()\"" '))
+    data = {
+        "Cliente" : values[0].strip('()\"" '),
+        "Linea": lineas
+    }
+    # print(values)
+    # data = {}
+    # for k, v in zip(keys, values):
+    #     v = v.strip("\"")
+    #     if k == "Cliente" or k == "Producto_entregado":
+    #         v = v.strip('()\"" ')
+    #     data[k] = v
+    # json_data.append(data)
+
+    return data
