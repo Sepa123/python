0de76172c8d997cac71ce22f7a0c1b96237b8db6
Sepa123 <sebastian.retamal.contreras@gmail.com>
2023-07-24 08:30:52 -0400

    Update clientes.py

--- routers/clientes.py
+++ routers/clientes.py
@@ -17,7 +17,7 @@ async def registrar_retiro_clientes(retiro_cliente : RetiroCliente):
     try:
         results = conn.get_ruta_manual(retiro_cliente.Codigo_pedido)
         data = retiro_cliente.dict()
-        conn.registrar_retiro_cliente(data)
+        # conn.registrar_retiro_cliente(data)
         return results
     except:
          raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Error")
