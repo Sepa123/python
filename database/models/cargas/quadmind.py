f7ff4aa1b4cf1930b637a59753ce654e6df12d71
Sepa123 <sebastian.retamal.contreras@gmail.com>
2023-07-28 10:32:03 -0400

    Update quadmind.py

--- database/models/cargas/quadmind.py
+++ database/models/cargas/quadmind.py
@@ -13,7 +13,7 @@ class CargaQuadmind(BaseModel):
     Telefono: str
     Email: str
     Codigo_pedido: str
-    Fecha_pedido: datetime
+    Fecha_pedido: str
     Operacion: str
     Codigo_producto: str
     Descripcion_producto: str
