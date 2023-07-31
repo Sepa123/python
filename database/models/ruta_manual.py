432e9d5edb77af52b135cfbb240e717a6f01326a
Sepa123 <sebastian.retamal.contreras@gmail.com>
2023-07-31 10:36:25 -0400

    Update ruta_manual.py

--- database/models/ruta_manual.py
+++ database/models/ruta_manual.py
@@ -15,15 +15,15 @@ class RutaManual(BaseModel):
     Email: str
     Codigo_pedido: str
     Fecha_pedido: str
-    Operacion: str
+    Operacion: Optional[str]
     Codigo_producto: str
     Descripcion_producto: str
     Cantidad_producto: int
-    Peso: int
-    Volumen: int
-    Dinero: int
-    Duracion_min: int
-    Ventana_horaria_1: str
+    Peso: Optional[str]
+    Volumen: Optional[int]
+    Dinero: Optional[int]
+    Duracion_min: Optional[int]
+    Ventana_horaria_1: Optional[str]
     Ventana_horaria_2: Optional[str]
     Notas: Optional[str]
     Agrupador: Optional[str]
