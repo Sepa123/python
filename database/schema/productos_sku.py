5b4192c13628f19548bdd94895069d472f777456
Sepa123 <sebastian.retamal.contreras@gmail.com>
2023-07-31 15:53:30 -0400

    Update productos_sku.py

--- database/schema/productos_sku.py
+++ database/schema/productos_sku.py
@@ -1,5 +1,5 @@
 def producto_sku(producto):
-    return  {
+    return {
         "Codigo_cliente": producto[0],
         "Nombre": producto[1],
         "Calle": producto[2],
@@ -8,25 +8,30 @@ def producto_sku(producto):
         "Latitud": producto[5],
         "Longitud": producto[6],
         "Telefono": producto[7],
-        "Email":producto[8],
-        "Codigo_pedido":producto[9],
+        "Email": producto[8],
+        "Codigo_pedido": producto[9],
         "Fecha_pedido": producto[10],
         "Operacion": producto[11],
-        "Codigo_producto":  producto[12],
+        "Codigo_producto": producto[12],
         "Descripcion_producto": producto[13],
-        "Cantidad_producto":  producto[14],
+        "Cantidad_producto": producto[14],
         "Peso": producto[15],
         "Volumen": producto[16],
         "Dinero": producto[17],
-        "Duracion_min": producto[18], 
+        "Duracion_min": producto[18],
         "Ventana_horaria_1": producto[19],
         "Ventana_horaria_2": producto[20],
         "Notas": producto[21],
-        "Agrupador" : producto[22],
-        "Email_remitentes" : producto[23],
-        "Eliminar_pedido" : producto[24],
-        "Vehiculo" : producto[25],
-        "Habilidades" : producto[26]
+        "Agrupador": producto[22],
+        "Email_remitentes": producto[23],
+        "Eliminar_pedido": producto[24],
+        "Vehiculo": producto[25],
+        "Habilidades": producto[26],
+        "SKU": producto[27],
+        "Pistoleado": producto[28],
+        "Tamaño": producto[29],
+        "Estado": producto[30],
+        "En_ruta": producto[31]
     }
 
 def productos_sku_schema(productos):
