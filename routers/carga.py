9b95ded79ccbc8dfe3102aae976a64f87cedc8ef
Sepa123 <sebastian.retamal.contreras@gmail.com>
2023-08-16 12:57:30 -0400

    update

--- routers/carga.py
+++ routers/carga.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, status,HTTPException, UploadFile, File
 from typing import List
 import pandas as pd
 import os 
+import time
 
 from datetime import datetime
 ##Modelos
@@ -41,6 +42,14 @@ async def get_carga_quadminds():
 
     return pedidos_planificados_schema(results)
 
+@router.get("/quadminds/buscar/pedido_planificados" , status_code=status.HTTP_202_ACCEPTED)
+async def get_carga_quadminds():
+    results = conn.get_pedido_planificados_quadmind_by_cod_pedido()
+
+    print(len(results))
+
+    return results[0]
+
 @router.post("/quadminds/subir-archivo", status_code=status.HTTP_202_ACCEPTED)
 async def subir_archivo(id_usuario : str, file: UploadFile = File(...)):
 
@@ -63,11 +72,18 @@ async def subir_archivo(id_usuario : str, file: UploadFile = File(...)):
 
     for i, data in enumerate(lista):
         # print(f"codigo cliente : {data['Código cliente']}, producto : {data['Producto']}, codigo pedido : {data['Codigo de Pedido']}")
+        # cantidad_encontrada = conn.get_pedido_planificados_quadmind_by_cod_pedido()
+        # if cantidad_encontrada[0] >= 1:
+        #     print("Producto ya esta registrado") 
+        # else:
         direccion = data['Domicilio']
         direccionesEX.append(direccion)
         posicion = i + 1
         conn.write_pedidos_planificados(data ,posicion, direccion)
         print(posicion)
+
+    
+    time.sleep(8)
 
     fecha_hora_actual = datetime.now()
 
@@ -75,32 +91,43 @@ async def subir_archivo(id_usuario : str, file: UploadFile = File(...)):
 
     fecha_hora_formateada = fecha_hora_actual.strftime("%Y%m%d%H%M")
    
-    # error = conn.asignar_ruta_quadmind_manual(id_usuario, fecha_hora_formateada)
+    # # # return {
+    # # #             "filename": file.filename, 
+    # # #             "message": "archivo", 
+    # # #             "codigos": "",
+    # # #             # "tiempo": diferencia[0][0],
+    # # #             "termino" : True ,
+    # # #             "error" : 0,
+    # # #             "ruta" : ruta,
+    # # #             "direcciones": direccionesEX
+    # # #             }
+   
+    error = conn.asignar_ruta_quadmind_manual(id_usuario, fecha_hora_formateada)
 
-    # diferencia = conn.calcular_diferencia_tiempo(fecha_dia)
+    diferencia = conn.calcular_diferencia_tiempo(fecha_dia)
 
     # error 1 : codigos inexistentes
 
-    # if error[0][0] == 1:
-    return {
-            "filename": file.filename, 
-            "message": "Error al subir el archivo", 
-            "codigos": f"2,3,4",
-            # "tiempo": diferencia[0][0],
-            "termino" : True ,
-            "error" : 1,
-            "ruta" : ruta,
-            "direcciones": direccionesEX
-            }
-    # else:
-    #     return {"filename": file.filename, 
-    #             "message": error[0][1], 
-    #             "codigos": "",
-    #             # "tiempo": diferencia[0][0],
-    #             "termino" : True,
-    #             "error" : 0,
-    #             "ruta" : ruta
-    #             }
+    if error[0][0] == 1:
+        return {
+                "filename": file.filename, 
+                "message": "Error al subir el archivo", 
+                "codigos": f"{error[0][1]}",
+                "tiempo": diferencia[0][0],
+                "termino" : True ,
+                "error" : 1,
+                "ruta" : ruta,
+                "direcciones": direccionesEX
+                }
+    else:   
+        return {"filename": file.filename, 
+                "message": error[0][1], 
+                "codigos": "",
+                "tiempo": diferencia[0][0],
+                "termino" : True,
+                "error" : 0,
+                "ruta" : ruta
+                }
 
 @router.post('/quadminds/asignar')
 async def asignar_ruta(id_usuario : int):
