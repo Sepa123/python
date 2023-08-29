a1765465b425ea874b1d8dad6721bc3b7fa18928
Sepa123 <sebastian.retamal.contreras@gmail.com>
2023-08-29 15:38:19 -0400

    Update rutas_de_pendientes.py

--- database/schema/rutas/rutas_de_pendientes.py
+++ database/schema/rutas/rutas_de_pendientes.py
@@ -1,17 +1,31 @@
 def ruta_de_pendientes(ruta):
-    return {
-		"origen" : ruta[0],
-		"guia" : ruta[1],
-		"fec_ingreso" : ruta[2],
-		"fec_pedido" : ruta[3],
-		"region" : ruta[4],
-		"comuna" : ruta[5],
-		"descripcion" : ruta[6],
-		"cant" : ruta[7],
-		"estado" : ruta[8],
-		"subestado" : ruta[9],
-		"verified" : ruta[10],
-		"recepcion" : ruta[11]
+    # return {
+	# 	"Origen" : ruta[0],
+	# 	"Guia" : ruta[1],
+	# 	"Fecha_ingreso" : ruta[2],
+	# 	"Fecha_pedido" : ruta[3],
+	# 	"Region" : ruta[4],
+	# 	"Comuna" : ruta[5],
+	# 	"Descripcion" : ruta[6],
+	# 	"Cantidad" : ruta[7],
+	# 	"Estado" : ruta[8],
+	# 	"Subestado" : ruta[9],
+	# 	"Verificado" : ruta[10],
+	# 	"recepcion" : ruta[11]
+	# }
+	return {
+	"Origen": ruta[0],
+	"Cod_entrega": ruta[1],
+	"Fecha_ingreso": ruta[2],
+	"Fecha_compromiso": ruta[3],
+	"Region": ruta[4],
+	"Comuna": ruta[5],
+	"Descripcion": ruta[6],
+	"Bultos": ruta[7],
+	"Estado": ruta[8],
+	"Subestado": ruta[9],
+	"Verificado": ruta[10],
+	"Recibido": ruta[11]
 	}
 
 def rutas_de_pendientes_schema(rutas):
