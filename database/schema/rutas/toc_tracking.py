8593b0ef0e79d3f96b671f137d93e88f55c8684f
Sepa123 <sebastian.retamal.contreras@gmail.com>
2023-08-31 13:00:34 -0400

    Update toc_tracking.py

--- database/schema/rutas/toc_tracking.py
+++ database/schema/rutas/toc_tracking.py
@@ -1,9 +1,9 @@
 def toc_tracking(toc):
     return {
-		"Fecha_Hora" : toc[0],
+		"Fecha_hora" : toc[0],
 		"Comuna" : toc[1],
 		"Direccion" : toc[2],
-		"Fecha_Compromiso" : toc[3],
+		"Fecha_compromiso" : toc[3],
 		"Observacion" : toc[4],
 		"Codigo_TY": toc[5],
 		"Creado_por" : toc[6]
