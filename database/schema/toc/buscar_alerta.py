f08cf86c0a33ac49f133001a7229f6dd6e219584
Sepa123 <sebastian.retamal.contreras@gmail.com>
2023-09-04 12:49:06 -0300

    Update buscar_alerta.py

--- database/schema/toc/buscar_alerta.py
+++ database/schema/toc/buscar_alerta.py
@@ -1,13 +1,13 @@
 def buscar_alerta(alerta):
     return {
-		"alerta" : alerta[0],
-		"fec_reprogramada" : alerta[1],
-		"direccion_correcta" : alerta[2],
-		"comuna_correcta" : alerta[3],
-		"subestado" : alerta[4],
-		"subestado_esperado" : alerta[5],
-		"observacion" : alerta[6],
-		"codigo1" : alerta[7]
+		"Alerta" : alerta[0],
+		"Fecha_Reprogramada" : alerta[1],
+		"Direccion_correcta" : alerta[2],
+		"Comuna_correcta" : alerta[3],
+		"Subestado" : alerta[4],
+		"Subestado_esperado" : alerta[5],
+		"0bservacion" : alerta[6],
+		"Codigo1" : alerta[7]
 	}
 
 def buscar_alertas_schema(alertas):
