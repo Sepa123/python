34970a21ee550d93e8b015db39e332589da7dad5
Sepa123 <sebastian.retamal.contreras@gmail.com>
2023-09-04 14:39:36 -0300

    update

--- database/schema/toc/buscar_alerta.py
+++ database/schema/toc/buscar_alerta.py
@@ -8,8 +8,7 @@ def buscar_alerta(alerta):
 		"Subestado_esperado" : alerta[5],
 		"Observacion" : alerta[6],
 		"Codigo1" : alerta[7],
-        "Direccion" : alerta[8],
-        "Comuna" : alerta[9]
+        "Comuna" : alerta[8]
 	}
 
 def buscar_alertas_schema(alertas):
