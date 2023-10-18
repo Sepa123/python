3a14c66cfc4d628739eab62462b4cce9834964c5
Sepa123 <sebastian.retamal.contreras@gmail.com>
2023-10-18 17:09:04 -0300

    Update beetrack.py

--- routers/beetrack.py
+++ routers/beetrack.py
@@ -52,7 +52,7 @@ async def post_dispatch(body : Dispatch, headers: tuple = Depends(validar_encabe
     
     print("datos tags", datos_tags)
     
-    if data["dispatch"] != 'dispatch':
+    if data["resource"] != 'dispatch':
         print("total datos de create",data)
     else:
         print("total datos de update",data)
