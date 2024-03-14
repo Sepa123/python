9eac780f6799bca117b8556d21697c4783fe9437
Sepa123 <sebastian.retamal.contreras@gmail.com>
2024-03-14 11:41:04 -0300

    Update bodega_virtual.py

--- database/schema/log_inversa/bodega_virtual.py
+++ database/schema/log_inversa/bodega_virtual.py
@@ -1,14 +1,14 @@
 def bodega_virtual(bodega):
     return {
-		"ingreso_li" : bodega[0],
-		"entrega" : bodega[1],
-		"fecha_pedido" : bodega[2],
-		"fec_original_pedido" :bodega[3],
-		"descripcion" : bodega[4],
-		"cantidad" : bodega[5],
-		"comuna" : bodega[6],
-		"region" : bodega[7],
-		"pistoleado" : bodega[0]
+		"Ingreso_li" : bodega[0],
+		"Entrega" : bodega[1],
+		"Fecha_pedido" : bodega[2],
+		"Fec_original_pedido" :bodega[3],
+		"Descripcion" : bodega[4],
+		"Cantidad" : bodega[5],
+		"Comuna" : bodega[6],
+		"Region" : bodega[7],
+		"Pistoleado" : bodega[0]
 	}
 
 
