5fdf79ef797ffe14b70810056ad9576e120fdb5c
Sepa123 <sebastian.retamal.contreras@gmail.com>
2024-03-20 09:32:30 -0300

    Update clientes.py

--- routers/clientes.py
+++ routers/clientes.py
@@ -14,12 +14,14 @@ conn = reportesConnection()
 @router.post("/retiro/registrar", status_code=status.HTTP_202_ACCEPTED)
 async def registrar_retiro_clientes(retiro_cliente : RetiroCliente):
     try:
-        results = conn.get_ruta_manual(retiro_cliente.Codigo_pedido)
+        # results = conn.get_ruta_manual(retiro_cliente.Codigo_pedido)
         data = retiro_cliente.dict()
         # print("retiro cliente datos :",retiro_cliente)
         # existe_registro = conn.find_retiro_cliente_existente(retiro_cliente.Codigo_pedido)
         # print(existe_registro == [])
         conn.registrar_retiro_cliente(data)
-        return results
+        return {
+            'message' : 'se registro correctamente'
+        }
     except:
          raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Error")
