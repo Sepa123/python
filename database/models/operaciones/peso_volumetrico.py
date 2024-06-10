9ea66fa63d611d37cb178efc240384b8487da1d0
Sepa123 <sebastian.retamal.contreras@gmail.com>
2024-06-10 15:08:37 -0400

    update

--- database/models/operaciones/peso_volumetrico.py
+++ database/models/operaciones/peso_volumetrico.py
@@ -1,13 +1,13 @@
 from pydantic import BaseModel
 
 
-class PesoVolumetrico(BaseModel):
-    sku: str
-    descripcion: str
-    alto: int | float
-    ancho: int | float
-    profundidad: int | float
-    peso_kg : int | float
-    bultos : int
-    id_user : int
-    ids_user: str
+# class PesoVolumetrico(BaseModel):
+#     sku: str
+#     descripcion: str
+#     alto: int | float
+#     ancho: int | float
+#     profundidad: int | float
+#     peso_kg : int | float
+#     bultos : int
+#     id_user : int
+#     ids_user: str
