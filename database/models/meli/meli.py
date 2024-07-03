4182a0a56dffb5910b2511ee728a254d3b3d2c34
Sepa123 <sebastian.retamal.contreras@gmail.com>
2024-07-03 18:06:31 -0400

    Create meli.py

---
+++ database/models/meli/meli.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+
+class pv(BaseModel):
+    sku: str
+    descripcion: str
+    alto: int | float
+    ancho: int | float
+    profundidad: int | float
+    peso_kg : int | float
+    bultos : int
+    id_user : int
+    ids_user: str
+
+class agregarPatente(BaseModel):
+    id_user : int
+    ids_user: str
+    fecha: str
+    ruta_meli: str
+    id_ppu : int
+    id_operacion : int
+    id_centro_op : int
+    estado : int
+    
+class updateApp(BaseModel):
+    id: int
+    estado: bool
