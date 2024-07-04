feb045021829903d9f4f09d84694453ae22ea60f
Sepa123 <sebastian.retamal.contreras@gmail.com>
2024-07-04 16:58:39 -0400

    Update client.py

--- database/client.py
+++ database/client.py
@@ -9767,10 +9767,10 @@ SELECT *
                 (SELECT count(*) FROM transporte.usuarios u WHERE u.id_razon_social = col.id) AS tripulacion,
                 (SELECT array_agg(json_build_object('patente', v.ppu, 'tipo', v.tipo, 'gps', v.gps, 'disponible', v.disponible, 'habilitado', v.habilitado))
                     FROM transporte.vehiculo v
-                    WHERE v.razon_id = col.id) AS patentes
+                    WHERE v.razon_id = col.id) AS patentes,
                 (SELECT array_agg(json_build_object('nombre', u.nombre_completo, 'tipo', u.tipo_usuario, 'activo', u.activo ))
                     FROM transporte.usuarios u
-                    WHERE u.id_razon_social = col.id) AS usuarios
+                    WHERE u.id_razon_social = c.id) AS usuarios
             FROM transporte.colaborador col
             left join hela.rol r ON col.tipo_razon  = r.id 
             left join transporte.vehiculo v ON v.razon_id = col.id
@@ -9825,12 +9825,12 @@ SELECT *
                 pdf_contrato,
                 (SELECT count(*) FROM transporte.vehiculo v WHERE v.razon_id = col.id) AS vehiculos,
                 (SELECT count(*) FROM transporte.usuarios u WHERE u.id_razon_social = col.id) AS tripulacion,
-                (SELECT array_agg(json_build_object('patente', v.ppu, 'tipo', v.tipo, 'gps', v.gps, 'disponible', v.disponible, 'habilitado', v.habilitado)),
+                (SELECT array_agg(json_build_object('patente', v.ppu, 'tipo', v.tipo, 'gps', v.gps, 'disponible', v.disponible, 'habilitado', v.habilitado))
+                    FROM transporte.vehiculo v
+                    WHERE v.razon_id = col.id) AS patentes,
                 (SELECT array_agg(json_build_object('nombre', u.nombre_completo, 'tipo', u.tipo_usuario, 'activo', u.activo ))
-                        FROM transporte.usuarios u
-                        WHERE u.id_razon_social = col.id) AS usuarios
-            FROM transporte.vehiculo v
-            WHERE v.razon_id = col.id) AS patentes
+                    FROM transporte.usuarios u
+                    WHERE u.id_razon_social = c.id) AS usuarios    
             FROM transporte.colaborador col
             left join hela.rol r ON col.tipo_razon  = r.id 
             left join transporte.vehiculo v ON v.razon_id = col.id
