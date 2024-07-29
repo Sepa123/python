61ed9164bf3bc96529c4e12cb97d0563145fdf28
Sepa123 <sebastian.retamal.contreras@gmail.com>
2024-07-29 16:32:47 -0400

    Update client.py

--- database/client.py
+++ database/client.py
@@ -125,7 +125,7 @@ class UserConnection():
 
         with self.conn.cursor() as cur:
             cur.execute("""
-            SELECT id, full_name ,mail,"password" ,active ,rol_id, null as foto_perfil  FROM "user".users WHERE lower(mail) = lower(%(mail)s)
+            SELECT id, full_name ,mail,"password" ,active ,rol_id, null as foto_perfil  FROM "user".users WHERE lower(mail) = lower(%(mail)s) and active=true
             """, data)
             return cur.fetchone()
         
