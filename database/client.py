b6a227920d35b1ebff05ce23ff0105dd1af379dd
Sepa123 <sebastian.retamal.contreras@gmail.com>
2024-10-03 22:46:39 -0300

    Update client.py

--- database/client.py
+++ database/client.py
@@ -11375,9 +11375,71 @@ UPDATE mercadolibre.citacion SET estado={estado} WHERE fecha='{fecha}' AND id_pp
             from transporte.obtener_comentarios_reclutamiento({id});   
                       """)
             return cur.fetchone()
+        
+
+     ##############
+
+    def resumen_rutas_fecha_sup(self,fecha_ini,fecha_fin,usuario):
+        with self.conn.cursor() as cur:
+            cur.execute(f"""   
+            with resumen_sup as (
+            SELECT  mo.modalidad,mo.nombre AS operacion,co.centro AS centro_operacion,r.region_name AS region,
+                    mds.fecha,mds.id_ruta,mds.ppu,mds.driver,
+                    mds.kilometros,mds.p_avance,mds.avance,
+                    CASE 
+                        WHEN (SELECT maux.modalidad FROM operacion.modalidad_operacion maux WHERE maux.id = mo.id) = 'FM' THEN
+                            (SELECT array_agg(json_build_object('fm_total_paradas', daux.fm_total_paradas, 'fm_paqueteria_colectada', daux.fm_paqueteria_colectada, 'fm_estimados', daux.fm_estimados, 'fm_preparados', daux.fm_preparados,'fm_p_colectas_a_tiempo', daux.fm_p_colectas_a_tiempo,'fm_p_no_colectadas', daux.fm_p_no_colectadas))
+                            FROM mercadolibre.mae_data_supervisores daux
+                            WHERE daux.id = mds.id)
+                        WHEN (SELECT maux.modalidad FROM operacion.modalidad_operacion maux WHERE maux.id = mo.id) = 'LM' THEN 
+                            (SELECT array_agg(json_build_object('lm_fallido',  daux.lm_fallido, 'lm_pendiente', daux.lm_pendiente, 'lm_spr', daux.lm_spr, 'lm_entregas', daux.lm_entregas,'lm_tiempo_ruta', daux.lm_tiempo_ruta,'lm_estado', daux.lm_estado))
+                            FROM mercadolibre.mae_data_supervisores daux
+                            WHERE daux.id = mds.id)
+                    END AS campos_por_operacion,
+                    mds.valor_ruta,
+                    mds.ruta_cerrada 
+                FROM mercadolibre.mae_data_supervisores mds 
+                LEFT JOIN operacion.centro_operacion co ON (co.id = mds.id_centro_operacion AND co.id_op = mds.id_operacion)
+                LEFT JOIN operacion.modalidad_operacion mo ON mo.id = mds.id_operacion
+                LEFT JOIN public.op_regiones r ON r.id_region::INT8 = co.region
+                LEFT JOIN mercadolibre.citacion c ON c.ruta_meli::INTEGER = mds.id_ruta
+                WHERE mds.fecha BETWEEN '{fecha_ini}'::DATE AND '{fecha_fin}'::DATE
+                and ({usuario} = ANY(co.id_coordinador) or {usuario} in (select u.id from hela.usuarios u where u.rol_id in ('5','90')))
+                ORDER BY 1 DESC, 5 asc
+
+            )
+
+
+            
+        ---select * from mercadolibre.resumen_rutas_fecha_sup('20240901','20240930',158,0);   
+            
+        
+
+
+        select json_agg(
+                json_build_object(
+                'Modalidad',modalidad,
+                'Operacion', operacion,
+                'Centro_operacion', centro_operacion,
+                'Region', region,
+                'Fecha', fecha,
+                'Id_ruta', id_ruta,
+                'Ppu', ppu,
+                'Driver', driver,
+                'Kilometros', kilometros,
+                'P_avance', p_avance,
+                'Avance', avance,
+                'Campos_por_operacion', campos_por_operacion,
+                'Valor_ruta', valor_ruta,
+                'Ruta_cerrada', ruta_cerrada
+                )
+        ) as campo
+            from resumen_sup                              
+                      """)
+            return cur.fetchone() 
 
     ##############
-    
+
     def listar_rutas_meli(self,fecha_inicio,fecha_fin):
         with self.conn.cursor() as cur:
             cur.execute(f"""   
