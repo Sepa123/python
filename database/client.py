7b5c48d50ad292c2ea3176cfcc8fcccac31d9c41
Sepa123 <sebastian.retamal.contreras@gmail.com>
2025-03-20 17:09:38 -0300

    updagte

--- database/client.py
+++ database/client.py
@@ -10870,7 +10870,7 @@ SELECT *
     def insert_bitacora_meli(self,id_usuario: int, ids_usuario:str, modificacion: str, latitud : float , longitud: float , origen):
         with self.conn.cursor() as cur:
             cur.execute(f"""
-            INSERT INTO mercadolibre.bitacora_general(id_usuario,ids_usuario, modificacion, latitud, longitud, origen)VALUES('{id_usuario}', '{ids_usuario}', '{modificacion}', '{latitud}', '{longitud}, '{origen}');                     
+            INSERT INTO mercadolibre.bitacora_general(id_usuario,ids_usuario, modificacion, latitud, longitud, origen)VALUES('{id_usuario}', '{ids_usuario}', '{modificacion}', '{latitud}', '{longitud}', '{origen}');                     
               """)
         self.conn.commit()
 
