ca8c398936d8d20722f503c2f49c2675f76b0f59
Sepa123 <sebastian.retamal.contreras@gmail.com>
2025-04-12 13:38:09 -0400

    Update beetrack_main.py

--- beetrack_main.py
+++ beetrack_main.py
@@ -1087,7 +1087,7 @@ async def post_dispatch_guide(request : Request , headers: tuple = Depends(valid
                         [{
                         "identifier": data.identifier,
                         "status_id": body_estados[0],
-                        "substatus": body_estados[1],
+                        # "substatus": body_estados[1],
                         "place": "CT Transyañez",
                         "is_trunk":  data.is_trunk,
                         "waypoint": {
@@ -1179,7 +1179,7 @@ async def post_dispatch_guide(request : Request , headers: tuple = Depends(valid
                                 [{
                                 "identifier": data.identifier,
                                 "status_id": body_estados[0],
-                                "substatus": body_estados[1],
+                                # "substatus": body_estados[1],
                                 "place": "CT Transyañez",
                                 "is_trunk":  data.is_trunk,
                                 "waypoint": {
@@ -1208,7 +1208,7 @@ async def post_dispatch_guide(request : Request , headers: tuple = Depends(valid
                                     [{
                                     "identifier": data.identifier,
                                     "status_id": body_estados[0],
-                                    "substatus": body_estados[1],
+                                    # "substatus": body_estados[1],
                                     "place": "CT Transyañez",
                                     "is_trunk":  data.is_trunk,
                                     "waypoint": {
