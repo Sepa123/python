f4142f25c98f8255007d924a074ce94e7fd65380
Sepa123 <sebastian.retamal.contreras@gmail.com>
2025-05-15 14:08:36 -0400

    Update pedidos.py

--- routers/pedidos.py
+++ routers/pedidos.py
@@ -154,7 +154,7 @@ async def get_rutas_de_pendientes_limitada():
         return {
             "Fecha_inicio" : result[0][0],
             "Fecha_fin" : result[0][1],
-            "Fecha_i_easy": result[1][1],
+            "Fecha_i_easy": result[1][0],
             "Fecha_i_fin" : result[1][1],
         }
      except Exception as e:
